c668b82192e48c7b19c9733ceb47a0283f4ec16a
unknown <douglas.nova@bandtec.com.br>
2020-09-05 16:05:29 -0300

    Projeto finalizado

---
+++ src/main/java/br/com/bandtec/projeto/sprint01/Valores.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bandtec.projeto.sprint01;
+
+/**
+ *
+ * @author AngeloBr12
+ */
+public class Valores {
+    Double ValorMin = 300000.0;
+    Double ValorMedia = 0.0;
+    Double ValorMax = 0.0;
+    Double valorAtual = 0.0;
+    Double valorTotal = 0.0;
+}
